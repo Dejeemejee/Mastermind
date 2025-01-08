d2b54067453f2aed81f130b0e99e4dd19c6ea7e9
Your Name <you@example.com>
2025-01-08 13:43:55 +0100

    add instructions file

---
+++ lib/instructions.rb
@@ -0,0 +1,9 @@
+#game instructions
+def get_game_instructions
+  puts "Welcome to Mastermind: a code breaking game between you and your computer"
+  puts "You can choose to be either code maker or code breaker"
+  puts "The code maker creates a 4 digit code using numbers from 1 to 4. Duplicates are allowed"
+  puts "The code breaker has to guess the exact code in under 12 turns, receiving hints at each turn"
+  puts "Can you beat the machine? Good luck!"
+  puts  "\r\n"
+end
